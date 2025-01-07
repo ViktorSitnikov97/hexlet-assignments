64edaafdcb1f037a0ba433c77c28e34f4109c16f
@hexlet/cli <support@hexlet.io>
2025-01-08 01:55:06 +0300

    submit java-spring-boot-ru/crud

---
+++ java-spring-boot-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,100 @@
+package exercise.controller;
+
+import java.util.HashSet;
+import java.util.List;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.mapper.ProductMapper;
+import exercise.repository.CategoryRepository;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.repository.ProductRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    // BEGIN
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping(path = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductDTO> index() {
+        var products = productRepository.findAll();
+        return products.stream()
+                .map(productMapper::map)
+                .toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO show(@PathVariable Long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        return productMapper.map(product);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO create(@Valid @RequestBody ProductCreateDTO productData) {
+        var categoryId = productData.getCategoryId();
+        var category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ConstraintViolationException(new HashSet<>()));
+
+        var product = productMapper.map(productData);
+        category.getProducts().add(product);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO update(@Valid @RequestBody ProductUpdateDTO productData, @PathVariable Long id) {
+        var categoryId = productData.getCategoryId().get();
+        var category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ConstraintViolationException(new HashSet<>()));
+
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        productMapper.update(productData, product);
+        category.getProducts().add(product);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        var category = categoryRepository.findByName(product.getCategory().getName())
+                        .orElseThrow(() -> new ResourceNotFoundException(
+                                "Category with name " + product.getCategory().getName() +" not found"));
+        category.getProducts().remove(product);
+        productRepository.deleteById(id);
+    }
+
+    // END
+}
